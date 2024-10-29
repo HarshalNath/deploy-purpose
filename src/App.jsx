c02ebfd53303d0c4cb889582c88677865d553e0b
Harshal Nath <harshalnath@protonmail.com>
2024-10-29 20:05:05 +0530

    trust componennt

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Howithelps from "./components/Howithelps";
 import Uses from "./components/Uses";
+import TrustComp from "./components/TrustComp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main>
       <Navbar />
       <Hero />
       <Howithelps />
       <Uses />
+      <TrustComp />
     </main>
   );
 }
